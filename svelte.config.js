e3728f96ce447ef60afcaf5932584c44b20ec55b
Douglas Fonseca dos Santos <douglas.fsantos89@gmail.com>
2022-01-28 17:18:05 -0300

    Adicionado vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +12,8 @@ const config = {
 		target: '#svelte',
 		files: {
 			assets: 'public'
-		}
+		},
+    adapter: vercel(),
 	}
 };
 
